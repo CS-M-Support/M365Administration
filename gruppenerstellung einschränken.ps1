094fc71b6edf4efef3d8ea0e9ad564b76a9eba76
Johnny Winkler <johnnyf.winkler@hotmail.com>
2022-04-26 09:19:59 +0200

    Create gruppenerstellung einschränken.ps1

---
+++ gruppenerstellung einschränken.ps1
@@ -0,0 +1,30 @@
+# Vorbereitung:
+# 1. Neue Sicherheitsgruppe mit dem Namen "GRP SECURITY GROUP CREATION" erstellen
+# 2. Mitglieder zur Gruppe hinzufügen (diejenigen in der Gruppe dürfen keine M365 Gruppen erstellen)
+
+$GroupName = "GRP SECURITY GROUP CREATION"
+$AllowGroupCreation = $False
+
+Connect-AzureAD
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+    $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+if($GroupName)
+{
+  $settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+} else {
+$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
+}
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
